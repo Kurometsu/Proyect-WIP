29cd3ff403483e073f5b7cb3267a05238e937560
19 <19>
2017-04-07 12:21:52 -0300

    Las modificaciones en clase

--- Programa1/Juego/Juego.cs
+++ Programa1/Juego/Juego.cs
@@ -11,6 +11,7 @@ namespace Juego
         private bool inGame = true;
         private int eleccion = 1;
         private char pj = 'O';
+        private char pj1 = '@';//agrego
         private char ob = '+';
         private char rv = '$';
 
@@ -18,6 +19,7 @@ namespace Juego
 
         private Random Gen = new Random();
         private Personaje playah = new Personaje();
+        private Personaje playah1 = new Personaje();//agrego
         private Soul[] Rivals = new Soul[5];
         private Obstaculo[] Obst = new Obstaculo[20];
 
@@ -46,7 +48,7 @@ namespace Juego
             //Menu
             while (eleccion != 3 || inGame == true) {
                 Console.SetCursorPosition(50, 5);
-            Console.WriteLine("-------Bienvenido------- \n[1]Iniciar\n[2]Creditos\n[3]Salir");
+            Console.WriteLine("-------Bienvenido------- \n[1]Iniciar 1 jugador\n[2]Iniciar 2 jugadores\n[3]Creditos\n[4]Salir");
                 //Chequeo de error
                 
                 try
@@ -81,26 +83,27 @@ namespace Juego
                         Console.Clear();
                         inGame = true;
                         playah.Start(50, 20, pj);
-                        while (inGame) {
-                            
+                        while (inGame)
+                        {
+
                             for (int i = 0; i < Obst.Length; i++)
                             {
                                 Obst[i].Show();
                             }
                             for (int i = 0; i < Rivals.Length; i++)
                             {
-                              
+
                                 Rivals[i].MoveLeft();
                                 Rivals[i].Show();
-                                
+
                             }
                             playah.Show();
-                            
+
                             //cki = Console.ReadKey();
                             if (Console.KeyAvailable)
                             {
                                 userKey = Console.ReadKey(true);
-                            
+
                                 switch (userKey.Key)
                                 {
 
@@ -129,9 +132,90 @@ namespace Juego
                                 }
                             }
 
+                            System.Threading.Thread.Sleep(50);
+                            Console.Clear();
+                        }
+                        break;
+                        //aca te agrego para 2 jugadores
+                    case 2:
+                        Console.WriteLine("Flechas para moverse\n Backspace para salir\n\n[0] Jugador\n[@] Jugador\n[+]=Obstaculo\n[$]=Enemigo\n\n(enter para continuar..) ");
+                        Console.ReadLine();
+                        Console.Clear();
+                        inGame = true;
+                        playah.Start(50, 20, pj);
+                        playah1.Start(0, 0, pj1);//agrego
+                        while (inGame)
+                        {
+
                             for (int i = 0; i < Obst.Length; i++)
                             {
-                                if (playah.getPosX() == Obst[i].getPosX() && playah.getPosY() == Obst[i].getPosY())
+                                Obst[i].Show();
+                            }
+                            for (int i = 0; i < Rivals.Length; i++)
+                            {
+
+                                Rivals[i].MoveLeft();
+                                Rivals[i].Show();
+
+                            }
+                            playah.Show();
+                            playah1.Show();//agrego
+
+                            //cki = Console.ReadKey();
+                            if (Console.KeyAvailable)
+                            {
+                                userKey = Console.ReadKey(true);
+
+                                switch (userKey.Key)
+                                {
+
+
+                                    case ConsoleKey.Backspace:
+                                        Console.Clear();
+                                        inGame = false;
+                                        break;
+
+                                    case ConsoleKey.UpArrow:
+                                        playah.MoveUp();
+
+                                        break;
+                                    case ConsoleKey.DownArrow:
+
+                                        playah.MoveDown();
+
+                                        break;
+                                    case ConsoleKey.RightArrow:
+                                        playah.MoveRight();
+
+                                        break;
+                                    case ConsoleKey.LeftArrow:
+                                        playah.MoveLeft();
+                                        break;
+                                    //aca te agrego para el otro pj
+                                    case ConsoleKey.W:
+                                        playah1.MoveUp();
+
+                                        break;
+                                    case ConsoleKey.S:
+
+                                        playah1.MoveDown();
+
+                                        break;
+                                    case ConsoleKey.D:
+                                        playah1.MoveRight();
+
+                                        break;
+                                    case ConsoleKey.A:
+                                        playah1.MoveLeft();
+                                        break;
+                                }
+                            }
+
+                            for (int i = 0; i < Obst.Length; i++)
+                            {
+                                //aca te agrego el ||
+                                if (playah.getPosX() == Obst[i].getPosX() && playah.getPosY() == Obst[i].getPosY()
+                                    || playah1.getPosX() == Obst[i].getPosX() && playah1.getPosY() == Obst[i].getPosY())
                                 {
                                     Console.Clear();
                                     Console.SetCursorPosition(50, 5);
@@ -144,7 +228,9 @@ namespace Juego
                             }
                             for (int i = 0; i < Rivals.Length; i++)
                             {
-                                if (playah.getPosX() == Rivals[i].getPosX() && playah.getPosY() == Rivals[i].getPosY())
+                                //aca te agrego el ||
+                                if (playah.getPosX() == Rivals[i].getPosX() && playah.getPosY() == Rivals[i].getPosY()
+                                    || playah1.getPosX() == Rivals[i].getPosX() && playah1.getPosY() == Rivals[i].getPosY())
                                 {
                                     Console.Clear();
                                     Console.SetCursorPosition(50, 5);
@@ -155,23 +241,29 @@ namespace Juego
                                 }
 
                             }
+                            //te agrego colision entre jugadores
+                            if (playah.getPosX() == playah1.getPosX() && playah.getPosY() == playah1.getPosY())
+                            {
+                                Console.Clear();
+                                Console.SetCursorPosition(50, 5);
+                                Console.WriteLine("GAME OVER\n\n\n(presione enter para continuar)");
+                                Console.ReadLine();
+                                inGame = false;
+                            }
+
                             System.Threading.Thread.Sleep(50);
                             Console.Clear();
                         }
-
-
-
-
                         break;
-                        //Creditos
-                    case 2:
+                    //Creditos
+                    case 3:
                         Console.SetCursorPosition(50, 5);
                         Console.WriteLine("By : Alejo Perez Loguzzo");
                         Console.ReadLine();
                         Console.Clear();
                         break;
                         //Salida
-                    case 3:
+                    case 4:
                         Console.Clear();
                         inGame = false;
                         break;
